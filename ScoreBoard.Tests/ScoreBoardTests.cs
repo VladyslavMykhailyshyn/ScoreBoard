da61d0f78be84b0946dd322257c85ccbe9c37b5e
Vladyslav_Mykhailyshyn <vladyslavmykhailyshyn@gmail.com>
2020-12-19 02:11:31 +0200

    Add Moq framework, implement tests for ScoreBoard class

--- ScoreBoard.Tests/ScoreBoardTests.cs
+++ ScoreBoard.Tests/ScoreBoardTests.cs
@@ -1,8 +1,131 @@
-﻿using System;
+﻿using Moq;
+using System.Collections.Generic;
+using ScoreBoard.Storage;
+using Xunit;
 
 namespace ScoreBoard.Tests
 {
     public class ScoreBoardTests
     {
+        [Theory]
+        [InlineData("Ukraine", "Poland")]
+        public void StartGameWithDefaultScoreTest(string team1, string team2)
+        {
+            //Arrange
+            var mockStorage = new Mock<IGamesStorage>();
+            mockStorage.Setup(p => p.AddGame(team1, team2))
+                .Verifiable();
+
+            //Act
+            var board = new ScoreBoard(mockStorage.Object);
+            board.StartGame(team1, team2);
+
+            //Assert
+            mockStorage.Verify(p => p.AddGame(team1,team2), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("Ukraine", "Poland", 1, 1)]
+        public void UpdateGameTest(string team1, string team2, int team1Score, int team2Score)
+        {
+            //Arrange
+            var mockStorage = new Mock<IGamesStorage>();
+            mockStorage.Setup(p => p.UpdateScore(team1, team2, team1Score, team2Score))
+                .Verifiable();
+
+            //Act
+            var board = new ScoreBoard(mockStorage.Object);
+            board.UpdateScore(team1,team2, team1Score, team2Score);
+
+            //Assert
+            mockStorage.Verify(p => p.UpdateScore(team1, team2, team1Score, team2Score), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("Ukraine", "Poland", 1, 1)]
+        public void StartGameWithCustomScoreTest(string team1, string team2, int team1Score, int team2Score)
+        {
+            //Arrange
+            var mockStorage = new Mock<IGamesStorage>();
+            mockStorage.Setup(p => p.AddGame(team1, team2))
+                .Verifiable();
+            mockStorage.Setup(p => p.UpdateScore(team1, team2, team1Score, team2Score))
+                .Verifiable();
+
+            //Act
+            var board = new ScoreBoard(mockStorage.Object);
+            board.StartGame(team1, team2, team1Score, team2Score);
+
+            //Assert
+            mockStorage.Verify(p => p.AddGame(team1, team2), Times.Once);
+            mockStorage.Verify(p => p.UpdateScore(team1, team2, team1Score, team2Score), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("Ukraine", "Poland")]
+        public void FinishGameTest(string team1, string team2)
+        {
+            //Arrange
+            var mockStorage = new Mock<IGamesStorage>();
+            mockStorage.Setup(p => p.DeleteGame(team1, team2))
+                .Verifiable();
+
+            //Act
+            var board = new ScoreBoard(mockStorage.Object);
+            board.FinishGame(team1, team2);
+
+            //Assert
+            mockStorage.Verify(p => p.DeleteGame(team1, team2), Times.Once);
+        }
+
+        [Theory]
+        [InlineData("Ukraine", "Poland", 1, 1)]
+        public void GetGamesSummaryTest(string team1, string team2, int team1Score, int team2Score)
+        {
+            //Arrange
+            var mockStorage = new Mock<IGamesStorage>();
+            var summaryMock = new Dictionary<(string homeTeam, string awayTeam), (int homeScore, int awayScore)>
+            {
+                {(team1, team2),(team1Score, team2Score)}
+            };
+            mockStorage.Setup(p => p.GetCurrentScores())
+                .Returns(summaryMock)
+                .Verifiable();
+
+            //Act
+            var board = new ScoreBoard(mockStorage.Object);
+            var summary = board.GetGamesSummary();
+
+            //Assert
+            mockStorage.Verify(p => p.GetCurrentScores(), Times.Once);
+            Assert.Equal(summaryMock, summary);
+        }
+
+        [Theory]
+        [InlineData("Ukraine", "Poland", 1, 1)]
+        public void ExportGamesSummaryTest(string team1, string team2, int team1Score, int team2Score)
+        {
+            //Arrange
+            var mockStorage = new Mock<IGamesStorage>();
+            var summaryMock = new Dictionary<(string homeTeam, string awayTeam), (int homeScore, int awayScore)>
+            {
+                {(team1, team2),(team1Score, team2Score)}
+            };
+            var exportExpected = new List<string>
+            {
+                $"{team1} {team1Score} - {team2} {team2Score}"
+            };
+            mockStorage.Setup(p => p.GetCurrentScores())
+                .Returns(summaryMock)
+                .Verifiable();
+
+            //Act
+            var board = new ScoreBoard(mockStorage.Object);
+            var exportResult = board.ExportGamesSummary();
+
+            //Assert
+            mockStorage.Verify(p => p.GetCurrentScores(), Times.Once);
+            Assert.Equal(exportExpected, exportResult);
+        }
     }
 }
--- ScoreBoard/ScoreBoard.cs
+++ ScoreBoard/ScoreBoard.cs
@@ -1,15 +1,84 @@
-﻿using ScoreBoard.Storage;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ScoreBoard.Storage;
 
 namespace ScoreBoard
 {
+    /// <summary>
+    /// Provides all functionality for a scoreboard
+    /// </summary>
     public class ScoreBoard
     {
         private readonly IGamesStorage _gamesStorage;
 
-        //It allows creating any other storage (SQL, document, another in-memory store) that implement the IGamesStorage interface and use it
+        /// 
+        /// <summary>
+        /// ScoreBoard controller. Creates ScoreBoard
+        /// Storage injection allows creating any other storage (SQL, document, another in-memory store)
+        /// that implements the IGamesStorage interface and uses it
+        /// </summary>
+        /// <param name="storage">Storage to store the scores for this scoreboard</param>
         public ScoreBoard(IGamesStorage storage)
         {
             _gamesStorage = storage;
+        }
+
+        /// <summary>
+        /// Starts a new game. The score is 0-0 by default, but the game can be added with another score (if needed)
+        /// </summary>
+        /// <param name="homeTeam">Home team name</param>
+        /// <param name="awayTeam">Away team name</param>
+        /// <param name="homeScore">Home team init score (zero by default)</param>
+        /// <param name="awayScore">Away team init score (zero by default)</param>
+        public void StartGame(string homeTeam, string awayTeam, int homeScore = 0, int awayScore = 0)
+        {
+            _gamesStorage.AddGame(homeTeam,awayTeam);
+            if (homeScore != 0 || awayScore != 0)
+            {
+                _gamesStorage.UpdateScore(homeTeam, awayTeam, homeScore, awayScore);
+            }
+        }
+
+        /// <summary>
+        /// Updates score for teams
+        /// </summary>
+        /// <param name="homeTeam">Home team name</param>
+        /// <param name="awayTeam">Away team name</param>
+        /// <param name="homeScore">Home team new score</param>
+        /// <param name="awayScore">Away team new score</param>
+        public void UpdateScore(string homeTeam, string awayTeam, int homeScore, int awayScore)
+        {
+            _gamesStorage.UpdateScore(homeTeam, awayTeam, homeScore, awayScore);
+        }
+
+        /// <summary>
+        /// Finishes existing game. Removes it from the scoreboard.
+        /// </summary>
+        /// <param name="homeTeam">Home team name</param>
+        /// <param name="awayTeam">Away team name</param>
+        public void FinishGame(string homeTeam, string awayTeam)
+        {
+            _gamesStorage.DeleteGame(homeTeam, awayTeam);
+        }
+
+        /// <summary>
+        /// Exports games summary as a set of strings in
+        /// '{homeTeamName} {homeTeamScore} - {awayTeamName} {awayTeamScore}' format
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> ExportGamesSummary()
+        {
+            return _gamesStorage.GetCurrentScores().Select(s =>
+                $"{s.Key.homeTeam} {s.Value.homeScore} - {s.Key.awayTeam} {s.Value.awayScore}");
+        }
+
+        /// <summary>
+        /// Returns games summary in a format convenient for further work
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<(string homeTeam, string awayTeam), (int homeScore, int awayScore)> GetGamesSummary()
+        {
+            return _gamesStorage.GetCurrentScores();
         }
     }
 }
